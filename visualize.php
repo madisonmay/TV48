08e1f0623f2ff244a3dbe5e9e59a4d915dfb67d7
Madison May <madison.may@students.olin.edu>
2013-06-19 14:26:31 -0400

    Added system for prediction of yearly CO2/energy expenditure based on existing average data.

--- visualize.php
+++ visualize.php
@@ -5,6 +5,8 @@
     <title>TV48</title>
     <meta charset="utf-8">
     <? include('base.php'); ?>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/d3/2.10.0/d3.v2.min.js"></script>
+    <script src="nv.d3.js"></script>
     <style>
 
         .thin-wrapper {
@@ -19,6 +21,14 @@
             text-align: center;
         }
 
+        #chart svg {
+          height: 675px;
+          width: 1450px;
+          margin-left: auto;
+          margin-right: auto;
+          margin-bottom: 50px;
+        }
+
     </style>
     <!--[if lt IE 9]>
         <style>
@@ -30,6 +40,129 @@
         </style>
     <![endif]-->
     <script>
+        nv.dev = false;
+        var average = [36, 35, 34, 21, 28, 28, 6, 9, 15, 31, 35, 24];
+        var actual = [40, 42, 36, 30];
+
+        function sum(array) {
+            var sum = 0;
+            for (var i=0; i<array.length; i++) {
+                sum = sum + array[i];
+            }
+            return sum;
+        }
+
+        function expected_absolute(actual, average) {
+            var result = actual.slice(0);
+            var difference = 0;
+            for (var i = 0; i < actual.length; i++) {
+                difference = difference + actual[i] - average[i];
+            }
+            var average_difference = difference/actual.length;
+            for (var i = actual.length; i < average.length; i++) {
+                var entry = average[i] + average_difference;
+                if (entry > 0) {
+                    result.push(entry); 
+                } else {
+                    result.push(0);
+                }
+            }
+            console.log("Absolute: ", result);
+            return result;
+        }
+
+        function expected_percentage(actual, average) {
+            var result = actual.slice(0);
+            var difference = 0;
+            for (var i = 0; i < actual.length; i++) {
+                difference = difference + actual[i]/average[i];
+            }
+            var average_difference = difference/actual.length;
+            for (var i = actual.length; i < average.length; i++) {
+                result.push(average[i]*average_difference);
+            }
+            console.log("Percentage: ", result);
+            return result;
+        }
+
+        function expected_hybrid(actual, average) {
+            var result = [];
+            var percentage = expected_percentage(actual, average);
+            var absolute = expected_absolute(actual, average);
+            for (var i = 0; i<absolute.length; i++) {
+                result.push((absolute[i] + percentage[i])/2);
+            }
+            console.log("Hybrid: ", result);
+            return result;
+        }
+
+        function paired(array) {
+            var result = [];
+            for (var i = 0; i < array.length; i++) {
+                result.push({x: i+1, y: array[i]});
+            }
+            return result;
+        }
+
+
+        $(document).ready(function() {
+            console.log("Actual: ", actual);
+            console.log("Average: ", average);
+            var absolute = expected_absolute(actual, average);
+            var percentage = expected_percentage(actual, average);
+            var hybrid = expected_hybrid(actual, average);
+
+            var absolute_object = paired(absolute);
+            var percentage_object = paired(percentage);
+            var hybrid_object = paired(hybrid);
+            var average_object = paired(average);
+
+            var expected_yearly = sum(hybrid);
+            var average_yearly = sum(average);
+            setTimeout(function() {co2(expected_yearly/average_yearly);}, 500);
+
+            nv.addGraph(function() {
+              var chart = nv.models.lineChart();
+
+              chart.xAxis
+                  .axisLabel('Month')
+                  .tickFormat(d3.format(',r'));
+
+              chart.yAxis
+                  .axisLabel('Energy Expenditure (KWh)')
+                  .tickFormat(d3.format('.02f'));
+
+              d3.select('#chart svg')
+                  .datum([
+                            {
+                              values: absolute_object,
+                              key: 'Absolute',
+                              color: '#ff7f0e'
+                            },
+                            {
+                              values: percentage_object,
+                              key: 'Percentage',
+                              color: '#2ca02c'
+                            }, 
+                            {
+                              values: hybrid_object,
+                              key: 'Hybrid',
+                              color: '#2f4f4f'
+                            }, 
+                            {
+                              values: average_object,
+                              key: 'Average',
+                              color: '#aa6600'
+                            }, 
+                         ])
+                  .transition().duration(500)
+                  .call(chart);
+
+              nv.utils.windowResize(chart.update);
+
+              return chart;
+            });
+        })
 
         function template(string,data){
             return string.replace(/%(\w*)%/g,function(m,key){
@@ -69,5 +202,8 @@
     <? include('header.php'); ?>
     CO2 Visualization
     <? include('header2.php'); ?>
+    <div id='chart'>
+        <svg></svg>
+    <div>
 </body>
 </html>
