28b65609f575c028b180a7afa4fc12fcfee9ab05
Madison May <madison.may@students.olin.edu>
2013-06-21 10:51:41 -0400

    Added quick.php -- a catalog of convenience functions

---
+++ quick.php
@@ -0,0 +1,12 @@
+<?
+//PHP convenience functions: quick.php
+
+function send($variable, $varname, $global = 1) {
+	if ($global) {
+		echo("<script> window" . $varname . " = " . json_encode($variable) . "</script>");
+	} else {
+		echo("<script> var " . $varname . " = " . json_encode($variable) . "</script>");
+	}
+}
+
+?>
