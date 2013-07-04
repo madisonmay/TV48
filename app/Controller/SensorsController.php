721f4c4e6dd4b8d761f0a281935f6cb8aa31c780
Madison May <madison.may@students.olin.edu>
2013-07-04 10:38:19 -0400

    Fixing incorrectly named files

--- app/Controller/SensorsController.php
+++ app/Controller/SensorsController.php
@@ -12,17 +12,17 @@
 		}
 
 		public function lighting() {
-			//Potentially a case of mixing presentation and logic?
-			$this->set('cssIncludes', array());
-			$this->set('jsIncludes', array('http://cdnjs.cloudflare.com/ajax/libs/d3/2.10.0/d3.v2.min.js', 'nv.d3')); 
+
 		}
 
 		public function heating() {
+			//Potentially a case of mixing presentation and logic? 
 			$this->set('cssIncludes', array());
 			$this->set('jsIncludes', array('http://cdnjs.cloudflare.com/ajax/libs/d3/2.10.0/d3.v2.min.js', 'nv.d3')); 
 		}
 
 		public function electricity() {
+			//Potentially a case of mixing presentation and logic? 
 			$this->set('cssIncludes', array());
 			$this->set('jsIncludes', array('http://cdnjs.cloudflare.com/ajax/libs/d3/2.10.0/d3.v2.min.js', 'nv.d3')); 
 		}
