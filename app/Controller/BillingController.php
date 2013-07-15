837f3e15573ea27d6861660a671a674ff34d6ef4
Madison May <madison.may@students.olin.edu>
2013-07-15 05:29:46 -0400

    Addition of skeleton BillingController

---
+++ app/Controller/BillingController.php
@@ -0,0 +1,19 @@
+<?
+	class HomeController extends AppController {
+
+		function beforeFilter() {
+			parent::beforeFilter();
+			$this->Auth->allow('index');
+		}
+
+		public function isAuthorized($user) {
+			if (in_array('landlord', $this->Session->read('User.roles')) {
+				return true;
+			} 
+			if (in_array('admin', $this->Session->read('User.roles')) {
+				return true;
+			} 
+			return false;
+		}
+	}
+?>
