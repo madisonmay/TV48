c2edf837dd8eb55cc868fc06f3a39d785fbdebe1
Madison May <madison.may@students.olin.edu>
2013-07-19 16:28:27 -0400

    Removed unnecessary PNGs

--- app/webroot/js/plot_formatting.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-	//very sketchy, but currently necessary
-	var pos = parseFloat($('.nv-x g g .nv-axislabel').attr('x')) - 30;
-	setTimeout(function() {$('.nv-x g g .nv-axislabel').attr('x', pos)}, 100);
-})
